5df4593aac7a2ea1c63e2ff2433dd8d6c1d8b702
Wellbek <louis.wellmeyer@gmail.com>
2023-04-14 13:34:58 +0200

    Project Setup

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
