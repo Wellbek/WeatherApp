d4303f588b7021146fd5b3cf5c05b6db592e7325
Wellbek <louis.wellmeyer@gmail.com>
2023-04-16 23:09:56 +0200

    Added location and degree text - set exemplary city, country and degree. No API yet

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,51 @@ class WeatherApp extends StatelessWidget {
           children: <Widget> [
             const GradientMaterial(),
             SearchBar(),
+            Container(
+              margin: const EdgeInsets.symmetric( vertical: 140,),
+              child: Column(     
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(Icons.location_on_outlined, color: Colors.white,),
+                      Text('Aachen', 
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  const Text('Germany', 
+                    style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric( vertical: 230,),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text('22°', 
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 128,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
+            )
           ],
         )
       )
--- lib/widgets/searchbar.dart
+++ lib/widgets/searchbar.dart
@@ -19,7 +19,7 @@ class _SearchBarState extends State<SearchBar> {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(
-        vertical: 70,
+        vertical: 55,
         horizontal: MediaQuery.of(context).size.width * .05,
       ),
       child: Material(
@@ -27,23 +27,23 @@ class _SearchBarState extends State<SearchBar> {
         borderRadius: BorderRadius.circular(15),
         color: Colors.white,
         child: TextField(
-          style: TextStyle(color: Colors.black),
+          style: const TextStyle(color: Colors.black),
           maxLines: 1,
           controller: _textController,
           decoration: InputDecoration(
-            hintStyle: TextStyle(color: Colors.grey),
+            hintStyle: const TextStyle(color: Colors.grey),
             errorText: _validate ? null : null,
             border: InputBorder.none,
             focusedBorder: InputBorder.none,
             enabledBorder: InputBorder.none,
-            icon: Padding(
-              padding: const EdgeInsets.only(left: 10),
+            icon: const Padding(
+              padding: EdgeInsets.only(left: 10),
               child: Icon(
                 Icons.search,
                 color: Colors.blue,
               ),
             ),
-            contentPadding: EdgeInsets.only(
+            contentPadding: const EdgeInsets.only(
               left: 0,
               bottom: 11,
               top: 11,
